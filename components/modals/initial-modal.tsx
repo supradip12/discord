48c8793379c0187b2d423fe566da1a3720c3ed1c
Supra <supradipdey1@gmail.com>
2024-01-11 13:07:37 +0530

    uploadthis_completed

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -24,6 +24,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+// import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -77,7 +78,18 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO:IMAGE UPLOAD
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        {/* Setting uploading things and now creating folderfileupload. Also add api file in middleware to avoid error. This is given in uploadthings */}
+                        FileUpload
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
