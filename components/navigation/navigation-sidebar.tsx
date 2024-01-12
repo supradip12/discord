88df9dfaec003f5625917b823255d7fdbe12225d
Supra <supradipdey1@gmail.com>
2024-01-12 21:13:35 +0530

    navigation-sidebar-scroolarea-started

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import React from "react";
 import NavigationAction from "./navigation-action";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -23,6 +25,14 @@ const NavigationSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            {server.name}
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
