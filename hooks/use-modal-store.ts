1b3c0e8cda294f669f9a631c4bed9b1fa32a2270
Supra <supradipdey1@gmail.com>
2024-01-13 17:48:06 +0530

    create-server-modal

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,21 @@
+// to control all modal to our application
+
+import { Channel, ChannelType, Server } from "@prisma/client";
+import { create } from "zustand";
+
+export type ModalType = "createServer";
+
+interface ModalStore {
+  type: ModalType | null;
+  isOpen: boolean;
+  onOpen: (type: ModalType) => void;
+  onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+
+  isOpen: false,
+  onOpen: (type) => set({ isOpen: true, type }),
+  onClose: () => set({ type: null, isOpen: false }),
+}));
