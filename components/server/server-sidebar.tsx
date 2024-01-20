14ba066e816717e4c140fb0026da7cdd42aa2739
Supra <supradipdey1@gmail.com>
2024-01-20 19:57:52 +0530

    working-on-invite-modal

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../ui/scroll-area";
+import { ServerHeader } from "./server-header";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -61,10 +62,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
 
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
-      {/* <ServerHeader
-     server={server}
-     role={role}
-   /> */}
+      <ServerHeader server={server} role={role} />
       <ScrollArea className="flex-1 px-3"></ScrollArea>
     </div>
   );
