f7f3d4485bb6994cf63fb6d63929d633b0fbb673
Supra <supradipdey1@gmail.com>
2024-01-21 11:10:01 +0530

    invite-modal working

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -14,10 +14,17 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
+import { useState } from "react";
+
 import { useModal } from "@/hooks/use-modal-store";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { Label } from "@/components/ui/label";
+import { Copy, RefreshCw } from "lucide-react";
 
 export const InviteModal = () => {
   const { isOpen, onClose, type } = useModal();
+  // const origin = useOrigin();
 
   const isModalOpen = isOpen && type === "invite";
 
@@ -26,14 +33,36 @@ export const InviteModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Invite Friends
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
             Give your server a personality with a name and an image. You can
             always change it later.
           </DialogDescription>
         </DialogHeader>
-        Invite Modal
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+          <div className="flex items-center mt-2 gap-x-2">
+            <Input
+              className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+              value="Invite Link"
+            />
+            <Button size="icon">
+              <Copy className="w-4 h-4" />
+            </Button>
+          </div>
+          <Button
+            variant="link"
+            size="sm"
+            className="text-xs text-zinc-500 mt-4"
+          >
+            Generate a new link
+            <RefreshCw className="w-4 h-4 ml-2" />
+            {/* Now create a hook to read the currenturl */}
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
