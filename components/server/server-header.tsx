cb61751841aae1d946bf0eafb659bedc7980c394
Supra <supradipdey1@gmail.com>
2024-01-27 19:35:33 +0530

    lever-deletecreatde

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -77,7 +77,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
           <DropdownMenuItem
-            onClick={() => {}}
+            onClick={() => onOpen("deleteServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Delete Server
