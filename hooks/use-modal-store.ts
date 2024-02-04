8104498b9a362cd69ffae07cf9173ddae3c8fa27
Supra <supradipdey1@gmail.com>
2024-02-04 18:41:56 +0530

    file-upload-withpdf

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -12,11 +12,14 @@ export type ModalType =
   | "leaveServer"
   | "deleteServer"
   | "deleteChannel"
-  | "editChannel";
+  | "editChannel"
+  | "messageFile";
 interface ModalData {
   server?: Server;
   channel?: Channel;
   channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
 }
 
 interface ModalStore {
---
+++ pages/api/socket/messages.ts
@@ -0,0 +1,92 @@
+import { NextApiRequest } from "next";
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { NextApiResponseServerIo } from "@/types";
+import { db } from "@/lib/db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIo
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "method not allowed" });
+  }
+
+  try {
+    const profile = await currentProfilePages(req);
+    const { serverId, channelId } = req.query;
+    const { content, fileUrl } = req.body;
+
+    if (!profile) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if (!serverId) {
+      return res.status(400).json({ error: "Server ID missing" });
+    }
+
+    if (!channelId) {
+      return res.status(400).json({ error: "Channel ID missing" });
+    }
+    if (!content) {
+      return res.status(400).json({ error: "Message Content is Missing." });
+    }
+
+    const server = await db.server.findFirst({
+      where: {
+        id: serverId as string,
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      include: {
+        members: true,
+      },
+    });
+
+    if (!server) {
+      return res.status(404).json({ error: "Server not found" });
+    }
+
+    const channel = await db.channel.findFirst({
+      where: {
+        id: channelId as string,
+        serverId: serverId as string,
+      },
+    });
+
+    if (!channel) {
+      return res.status(404).json({ error: "Channel not found" });
+    }
+
+    const member = server.members.find(
+      (member) => member.profileId === profile.id
+    );
+
+    if (!member) {
+      return res.status(404).json({ error: "Member not found" });
+    }
+    const message = await db.message.create({
+      data: {
+        content,
+        fileUrl,
+        channelId: channelId as string,
+        memberId: member.id,
+      },
+      include: {
+        member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    const channelKey = `chat:${channelId}:messages`;
+    res?.socket?.server?.io?.emit(channelKey, message);
+  } catch (error) {
+    console.log("[MESSAGE_ID]", error);
+    return res.status(500).json({ error: "Internal Error" });
+  }
+}
